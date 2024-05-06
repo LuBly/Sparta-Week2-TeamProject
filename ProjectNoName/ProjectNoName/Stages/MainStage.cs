4d60da69a31d5b78e1c0a4acb7e677316e54aeef
Park Hyunho <lumenize@ajou.ac.kr>
2024-05-06 12:30:17 +0900

    [Fix] 이미 죽은 몬스터에게 스킬 시전이 가능하던 현상 해결

--- ProjectNoName/ProjectNoName/Stages/MainStage.cs
+++ ProjectNoName/ProjectNoName/Stages/MainStage.cs
@@ -330,7 +330,15 @@ namespace ProjectNoName
             // 범위 내의 값을 선택한 경우
             else if (monsterIdx > 0 && monsterIdx < battleMonsters.Count)
             {
-                AttackMonster_Skill_2_3(monsterIdx, skillIdx);
+                if (battleMonsters[monsterIdx].Data.Health > 0) 
+                    AttackMonster_Skill_2_3(monsterIdx, skillIdx);
+                else
+                {
+                    Console.WriteLine("\n[이미 죽은 대상입니다!]");
+                    Thread.Sleep(500);
+                    ShowPlayerAttack();
+                }
+                
             }
             // 범위 밖의 값을 선택한 경우
             else
